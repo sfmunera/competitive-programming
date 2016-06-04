f4b20d749c233af7a4841a63f635dcea5cf4eb96
Sebastian Munera <sfmunera@gmail.com>
2016-06-05 01:14:23 +0200

    add new solutions for Div2 352

---
+++ codeforces/SummerCamp.java
@@ -0,0 +1,75 @@
+package codeforces;
+
+import java.io.*;
+import java.util.*;
+
+public class A {
+	public static void main(String[] args) {
+		InputReader in = new InputReader(System.in);
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= 1000; ++i) {
+			sb.append(i);
+		}
+		
+		String str = sb.toString();
+		
+		int n = in.nextInt();
+		
+		System.out.println(str.charAt(n - 1));
+	}
+	
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream));
+			tokenizer = new StringTokenizer("");
+		}
+
+		public String next() {
+			try {
+				while (!tokenizer.hasMoreTokens())
+					tokenizer = new StringTokenizer(reader.readLine());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			
+			return tokenizer.nextToken();
+		}
+		
+		public String nextLine() {
+			String line = null;
+			try {
+				tokenizer = new StringTokenizer("");
+				line =  reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			return line;
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		boolean hasNext() {
+            while (!tokenizer.hasMoreTokens()) {
+                String line = nextLine();
+                if (line == null) return false;
+                tokenizer = new StringTokenizer(line);
+            }
+            return true;
+        }
+	}
+}
