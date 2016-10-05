a0f9e72db978828d76847d5b9492111ee938e9ed
Sebastian Munera <sfmunera@gmail.com>
2016-10-05 20:23:15 +0200

    team practice BAPC 1

--- codeforces/Exams.java
+++ codeforces/Exams.java
@@ -1,71 +1,35 @@
 package codeforces;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Exams {
 	public static void main(String[] args) {
-		InputReader in = new InputReader(System.in);
-
+		Scanner in = new Scanner(System.in);
+		
 		int n = in.nextInt();
 		int k = in.nextInt();
 		
-		int ans = n;
-		for (int x = 0; x <= n; ++x)
-			for (int y = 0; y <= n; ++y)
-				for (int z = 0; z <= n; ++z)
-					for (int w = 0; w <= n; ++w)
-						if (x + y + z + w == n && 2 * x + 3 * y + 4 * z + 5 * w == k)
-							ans = Math.min(ans, x);
+		int[] grades = new int[n];
+		Arrays.fill(grades, 2);
+		int sum = 2 * n;
+		
+		int idx = 0;
+		while (sum < k) {
+			grades[idx]++;
+			sum++;
+			idx = (idx + 1) % n;
+		}
+		
+		int ans = 0;
+		for (int i = 0; i < n; ++i) {
+			if (grades[i] == 2) {
+				ans++;
+			}
+		}
+		
 		System.out.println(ans);
 		
+		in.close();
 		System.exit(0);
-	}
-	
-	static class InputReader {
-		public BufferedReader reader;
-		public StringTokenizer tokenizer;
-
-		public InputReader(InputStream stream) {
-			reader = new BufferedReader(new InputStreamReader(stream));
-			tokenizer = null;
-		}
-
-		public String next() {
-			try {
-				while (tokenizer == null || !tokenizer.hasMoreTokens())
-					tokenizer = new StringTokenizer(reader.readLine());
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-			
-			return tokenizer.nextToken();
-		}
-		
-		public String nextLine() {
-			String line = null;
-			try {
-				tokenizer = null;
-				line =  reader.readLine();
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-			return line;
-		}
-
-		public int nextInt() {
-			return Integer.parseInt(next());
-		}
-		
-		public double nextDouble() {
-			return Double.parseDouble(next());
-		}
-		
-		public long nextLong() {
-			return Long.parseLong(next());
-		}
 	}
 }
